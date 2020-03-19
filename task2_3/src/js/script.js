1ec0401d5420551772bcc583070c63018d3aa4f8
Alla <alla@redcollar.ru>
2020-03-19 20:00:28 +0300

    add form

--- task2_3/src/js/script.js
+++ task2_3/src/js/script.js
@@ -1,23 +1,29 @@
 "use strict";
 
-function myResize() {
-    let wrapper = document.getElementsByClassName('service-text');
-    // console.log(wrapperClass);
-    let elements = wrapper[0].querySelectorAll('div');
+document.addEventListener("DOMContentLoaded", function(event) {
+    function myResize() {
+        const wrapper = document.getElementsByClassName('wrapper-js');
+        let elements = wrapper[0].querySelectorAll('main-block-js');
 
-    let my_width = elements[0].clientWidth;
-    let my_height = elements[0].clientHeight;
+        let my_width = elements[0].clientWidth;
+        let my_height = elements[0].clientHeight;
 
-    for(let i=1; i < elements.length; i++) {
-        elements[i].style.width = my_width + "px";
-        elements[i].style.height = my_height + "px";
+        for(let i=1; i < elements.length; i++) {
+            elements[i].style.width = my_width + "px";
+            elements[i].style.height = my_height + "px";
+        }
     }
-}
 
+    // const wrapper_service = document.getElementsByClassName('wrapper-text');
+    // const wrapper_advantages_1 = document.getElementsByClassName('advantages_line1');
+    // const wrapper_advantages_2 = document.getElementsByClassName('advantages_line2');
 
-window.addEventListener('resize', function(event){
+    window.addEventListener('resize', function(event){
+        myResize();
+        // myResize(wrapper_advantages_1);
+        // myResize(wrapper_advantages_2);
+    });
     myResize();
+    // myResize(wrapper_advantages_1);
+    // myResize(wrapper_advantages_2);
 });
-
-myResize();
-
