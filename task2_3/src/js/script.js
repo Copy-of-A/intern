b6ba4b078a5556820ce37fda749d9a75fa1113db
Copy of A <moralobsession@gmail.com>
2020-03-24 11:54:03 +0300

    Add header-nav mobile

--- task2_3/src/js/script.js
+++ task2_3/src/js/script.js
@@ -58,8 +58,145 @@ document.addEventListener("DOMContentLoaded", function(event) {
     expand();
     hidePlaceholder();
     resizeShadows();
+    makeBurger();
 
     window.addEventListener('resize', function(event){
         resizeShadows();
     });
 });
+
+/**
+ * События формы
+ */
+function createForm(form) {
+    const action = form.action;
+    const elements = form.elements;
+
+    if(!action || !elements.length) return;
+
+    function validate(){
+        let result = true;
+
+        for(let i = 0; i< elements.length; i++){
+            let el = elements[i];
+            //if (!el.dataset.required) continue;
+            if (el.type === 'text' || el.type === 'tel' || el.type === 'email' || el.tagName === 'textarea'){
+                if (!el.value) {
+                    el.classList.add('error');
+                    el.addEventListener('input', removeClass);
+                    result = false;
+                }
+            }
+            if (el.type === 'select'){
+                console.log(el.type);
+                if (!el.value) {
+                    el.classList.add('error');
+                    console.log("error");
+                    el.addEventListener('input', removeClass);
+                    result = false;
+                }
+            }
+            else if (el.type === 'checkbox' || el.type === 'radio'){
+                if(!el.checked) {
+                    el.classList.add('error');
+                    el.addEventListener('change', removeClass);
+                    result = false;
+                }
+            }
+            else if (el.tagName.toLocaleLowerCase() ==='select' && !el.getAttribute('multiply')){
+                if (el.selectedIndex === 0) {
+                    el.classList.add('error');
+                    el.addEventListener('change', removeClass);
+                    result = false;
+                }
+            }
+        }
+
+        function removeClass(){
+            this.classList.remove('error');
+        }
+
+        return result;
+    }
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        if(!validate()) {
+            // h2.innerHTML = 'Ошибка!';
+            // p.innerHTML = 'Проверьте корректность вводимых данных';
+            // p.style.color = '#EC3332';
+        }
+        else{
+            // let btn_close = document.createElement('button');
+            // let div = document.createElement('div');
+            // let h2 = document.createElement('h2');
+            // let p = document.createElement('p');
+            // document.documentElement.classList.add("popupActive");
+            // document.documentElement.style.overflowY = 'hidden';
+            // const popup_inner = document.querySelector('.popup-inner');
+            // popup_inner.innerHTML = '';
+            // popup_inner.append(btn_close);
+            // btn_close.setAttribute("class", "close");
+            // popup_inner.append(div);
+            // div.append(h2);
+            // div.append(p);
+            // h2.innerHTML = 'Поздравляем!';
+            // p.innerHTML = 'Вы записались на RDCLR.HOME';
+            // h2.style.color = '#000000';
+            let myForm = document.querySelector('form');
+            myForm.reset();
+
+            // btn_close.addEventListener('click', function () {
+            //     document.documentElement.classList.remove('popupActive');
+            //     document.documentElement.style.overflowY = 'visible';
+            // });
+        }
+    })
+}
+
+function createFormWrapper(){
+    const forms = document.querySelectorAll('form');
+    const overlay = document.querySelector('.overlay');
+    if (!forms || !overlay) return;
+
+    overlay.addEventListener('click', function () {
+        document.documentElement.classList.remove('popupActive');
+        document.documentElement.style.overflowY = 'visible';
+
+    });
+
+    Array.from(forms).forEach(form => createForm(form))
+}
+
+/**
+ * Меню бургер для мобилки
+ */
+function makeBurger(){
+    let button = document.querySelector(".nav-button");
+    let nav = document.querySelector("nav");
+
+    if (!button || !nav) return;
+    let active = false;
+
+    let menu = nav.querySelector('.nav-menu');
+    let div = document.createElement('div');
+    let tel = document.createElement('span');
+    tel.innerText = "+7 (800) 700-06-07";
+    div.classList.add('tel-menu');
+    div.appendChild(tel);
+
+    button.addEventListener("click", function () {
+        if (!active) {
+            nav.classList.add("active");
+            document.documentElement.style.overflowY = 'hidden';
+            active = true;
+            menu.appendChild(div);
+        }
+        else{
+            nav.classList.remove("active");
+            document.documentElement.style.overflowY = 'visible';
+            active = false;
+            menu.removeChild(div)
+        }
+    });
+}
